550fbda379ce5745f477a7769b69cf279664aa03
tikendraw <tikendraksahu1029@gmail.com>
2024-09-11 13:09:18 +0530

    func: get_model_class, extract_model_class_and_model_name

--- llms/__init__.py
+++ llms/__init__.py
@@ -1,5 +1,29 @@
 from .testllm import TestLLM
 from .ollamallm import OllamaLLM 
 from .groqllm import GroqLLM
+from .utils import DotDict
 
-__all__ = ["TestLLM", "GroqLLM", "OllamaLLM"]
+models = {
+    'groq':GroqLLM,
+    'ollama':OllamaLLM,
+}
+
+def get_model_class(model_class:str):
+    model_class =  models.get(model_class, None)
+    if model_class:
+        return model_class  
+    else: 
+        raise ValueError("Model not supported")
+    
+
+def extract_model_class_and_model_name(model_str:str):
+    '''splits str from first / (slash)'''
+    try:
+        l = model_str.split('/')
+        model_class = l[0]
+        model_name = '/'.join(l[1:])
+        return model_class, model_name
+    except IndexError as e:
+        raise ValueError("Not a valid model name, do this: groq/llama3-8b-8192 or ollama/qwen2:0.5b")
+
+__all__ = ["TestLLM", "GroqLLM", "OllamaLLM", 'get_model_class', 'extract_model_class_and_model_name', 'DotDict']
