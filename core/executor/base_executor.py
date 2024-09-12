08560a371a98aa26908ac0b7e611a2af05e83b26
tikendraw <tikendraksahu1029@gmail.com>
2024-09-12 19:59:51 +0530

    func: clean_commands

--- core/executor/base_executor.py
+++ core/executor/base_executor.py
@@ -1,15 +1,15 @@
-# core/baseexecuter.py
-# Objective: Implements the BaseExecuter class for handling execution of commands and prints output
-
-from typing import List, Optional
-from termcolor import colored
-import subprocess
-
         
 class BaseExecutor:
     def __init__(self, command: str=None):
         self.command = command
         
+    def clean_commands(self, commands:list[str]) -> list[str]:
+        n_commands = []
+        for command in commands:
+            n_commands.append(self._clean_command(command))
+        
+        return n_commands
+            
         
     def run(self, verbose:bool=True,**kwargs):
         raise NotImplementedError('Implement run')        
@@ -19,3 +19,10 @@ class BaseExecutor:
     
     def __repr__(self):
         return f"BaseExecuter(command={self.command})"        
+    
+    def _clean_command(self, command:str) -> str:
+        # remove spaces
+        command = command.strip()
+        # remove ;
+        command = command.rstrip(';')
+        return command
