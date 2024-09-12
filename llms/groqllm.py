78e0f324afe74645a768191c80da86e818741367
tikendraw <tikendraksahu1029@gmail.com>
2024-09-12 20:00:35 +0530

    restructure

--- llms/groqllm.py
+++ llms/groqllm.py
@@ -1,43 +1,30 @@
-from groq import Groq
-from core.basellm import BaseLLM
+from core.basellm import BaseLLM, LLMConfig
+from typing import List, Dict, Any
 from core.prompts import SYSTEM_PROMPT
-from .utils import DotDict
 import os
-from core.basemessage import SystemMessage, UserMessage, AssistantMessage, LLMHistory
-from typing import Any
-from core.bashcode import BashCode
 
-class GroqLLM(BaseLLM):
+
+class GroqLLM(BaseLLM):    
     api_key_var_name = 'GROQ_API_KEY'
-    
-    def __init__(self, api_key: str=None, model_name: str = 'llama3-8b-8192', system_prompt: str = SYSTEM_PROMPT, **kwargs):
-        super().__init__()
-        self.client = Groq(api_key=api_key)
-        self.history = LLMHistory(system_prompt=system_prompt)
-        self.model_name = model_name
-        
-    def _response_parser(self, response:Any) -> str:
+
+    def __init__(self, config: LLMConfig):
+        super().__init__(config)
+        from groq import Groq
+        self.client = Groq(api_key=self.api_key)
+
+    def _response_parser(self, response: Any) -> str:
         try:
             return response.choices[0].message.content
         except AttributeError as e:
             print(response)
-            raise e    
-        
-    def _generate_response(self,user_prompt:str, history:list[dict]=None, **kwargs) -> Any:
-        response = self.client.chat.completions.create(
-            messages=[*history,
-                {
-                    "role": "user",
-                    "content": user_prompt,
-                },
-            ],
-            model=self.model_name,
+            raise e
+
+    def _generate_response(self, user_prompt: str, history: List[Dict[str, str]] = None, **kwargs) -> Any:
+        return self.client.chat.completions.create(
+            messages=[*history, {"role": "user", "content": user_prompt}],
+            model=self.config.model_name,
             **kwargs
         )
-        
-        return response
-    
-        
 
-    
-
+    def get_api_key_from_environment(self) -> str:
+        return os.getenv(self.api_key_var_name)
