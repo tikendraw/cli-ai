f5337193a2f2e34db9d8fe1a8de4039eb46b44d6
tikendraw <tikendraksahu1029@gmail.com>
2024-09-14 17:02:46 +0530

    typehint for run method

--- core/executor/base_executor.py
+++ core/executor/base_executor.py
@@ -1,28 +1,31 @@
-        
+from typing import Tuple
+
+
 class BaseExecutor:
-    def __init__(self, command: str=None):
+    def __init__(self, command: str = None):
         self.command = command
-        
-    def clean_commands(self, commands:list[str]) -> list[str]:
+
+    def clean_commands(self, commands: list[str]) -> list[str]:
         n_commands = []
         for command in commands:
             n_commands.append(self._clean_command(command))
-        
+
         return n_commands
-            
-        
-    def run(self, verbose:bool=True,**kwargs):
-        raise NotImplementedError('Implement run')        
-        
+
+    def run(
+        self, verbose: bool = True, **kwargs
+    ) -> Tuple[str, str, str, int]:  # command, stdout, stderr, exit_code
+        raise NotImplementedError("Implement run")
+
     def __str__(self):
         return f"BaseExecuter(command={self.command})"
-    
+
     def __repr__(self):
-        return f"BaseExecuter(command={self.command})"        
-    
-    def _clean_command(self, command:str) -> str:
+        return f"BaseExecuter(command={self.command})"
+
+    def _clean_command(self, command: str) -> str:
         # remove spaces
         command = command.strip()
         # remove ;
-        command = command.rstrip(';')
+        command = command.rstrip(";")
         return command
