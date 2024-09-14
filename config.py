20533e9f1247e4512d10425133c9ccfdadb8d33f
tikendraw <tikendraksahu1029@gmail.com>
2024-09-14 17:08:25 +0530

    added config file and default dictionary

--- config.py
+++ config.py
@@ -1,11 +1,20 @@
-
-# config.py
-# Objective: Stores configuration variables
-# - Loads environment variables and provides a central place for app settings
-
 import os
+import json
 
 config_folder = os.path.expanduser("~/.config/cliai")
 os.makedirs(config_folder, exist_ok=True)
 
+config_file = os.path.join(config_folder, "config.json")
 history_file = os.path.join(config_folder, "history.jsonl")
+
+
+config_dict = {
+    "default_model": "openai/gpt-3.5-turbo",
+    "api_key": None,
+    "ask_user": "always",  # always, never, sometimes (only when llms suggest it)
+    "retry_generation": 3,  # number of times to retry generation
+}
+
+if not os.path.exists(config_file):
+    with open(config_file, "w") as f:
+        f.writelines(json.dumps(config_dict, indent=2))
