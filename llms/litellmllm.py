a790c6e3df784ebd782ba75596e41f7130fd9e7a
tikendraw <tikendraksahu1029@gmail.com>
2024-09-14 17:00:55 +0530

    formatted litellm class

--- llms/litellmllm.py
+++ llms/litellmllm.py
@@ -6,19 +6,23 @@ from core.basemessage import LLMHistory
 from core.bashcode import BashCode
 from typing import Any
 
+
 class LitellmLLM(BaseLLM):
-    
-    api_key_var_name = 'some_api_key'
-    
-    def __init__(self, config:LLMConfig):
+
+    api_key_var_name = "some_api_key"
+
+    def __init__(self, config: LLMConfig):
         self.config = config
         self.history = LLMHistory(load_history=False)
 
-    def _generate_response(self, user_prompt:str, history:list[dict], **kwargs):
-        
-        return completion(model=self.config.model_name, messages=[*history, {'role':'user', 'content':user_prompt}], **kwargs)
-    
-    
+    def _generate_response(self, user_prompt: str, history: list[dict], **kwargs):
+
+        return completion(
+            model=self.config.model_name,
+            messages=[*history, {"role": "user", "content": user_prompt}],
+            **kwargs
+        )
+
     def _response_parser(self, response: Any) -> str:
         try:
             return response.choices[0].message.content
@@ -26,7 +30,5 @@ class LitellmLLM(BaseLLM):
             print(response)
             raise e
 
-
     def get_api_key_from_environment(self):
         return os.getenv(self.api_key_var_name)
-    
